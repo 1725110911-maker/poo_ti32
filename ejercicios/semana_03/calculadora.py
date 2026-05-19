561e165847b23b27a9612753bb511c69c92a9974
HECTOR EMILIANO CRUZ RAMIREZ <1725110911@utectulancingo.edu.mx>
2026-05-19 19:20:58 +0000

    UPDATE

---
+++ ejercicios/semana_03/calculadora.py
@@ -0,0 +1,14 @@
+class Calculadora:
+
+    def __init__(self):
+        pass
+
+    def suma(self, numero1, numero2):
+       resultado = numero1 + numero2
+       print(f"Resultado Suma: {resultado}")
+    def sumaEnteros(numero1, numero2):
+       resultado = numero1 + numero2
+       print(f"Resultado Suma: {resultado}")
+casio = Calculadora()
+casio.suma(10,15.5)
+casio.sumaEnteros("10". "15.5")
