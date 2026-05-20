00ab43f845c842e6a8b355ac00a9fcc9887c8a28
HECTOR EMILIANO CRUZ RAMIREZ <1725110911@utectulancingo.edu.mx>
2026-05-20 14:18:47 +0000

    UPDATED hola_mundo.py

--- ejercicios/semana_02/hola_mundo.py
+++ ejercicios/semana_02/hola_mundo.py
@@ -6,5 +6,25 @@ class NombreClase:
     def metodoUno(self):
         print("Metodo Uno")
 
+    def metodoDos(self, variable_uno:int, variable_dos:float) ->int:
+        """
+        Este metodo recibe 2 variables enteras, la suma y regresa el resultado de la suma
+    
+        Args:
+    
+        variable_uno:int - Primer numero entero
+        variable_dos:int - Segundo numero entero
+    
+        :return
+    
+        suma:int - Suma de los dos numeros enteros
+    
+        """
+    
+    def metodoTres(self, variable_tres:str)->None:
+        print(f"Número de caracteres: {len(variable_tres)}")
+
 nombre_objeto = NombreClase ()
 nombre_objeto.metodoUno()
+nombre_objeto.metodoDos(10, 15.5)
+nombre_objeto.metodoTres("Hola")
